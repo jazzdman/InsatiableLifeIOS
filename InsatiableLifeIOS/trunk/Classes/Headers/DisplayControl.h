c4c07cde46adf4380a20615b8264f1ee5bfbbc9f
Jason Mazzotta <jazzdman@gmail.com>
2014-05-10 17:06:26 +0000

    Used to resolve compiler warnings

---
+++ InsatiableLifeIOS/trunk/Classes/Headers/DisplayControl.h
@@ -0,0 +1,17 @@
+//
+//  ControllerProtocol.h
+//  InsatiableLifeIOS
+//
+//  Created by Jason Mazzotta on 4/24/14.
+//  Copyright (c) 2014 Full Plate Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DisplayControl <NSObject>
+
+-(void) controllerUp;
+-(void) controllerDown;
+-(void) viewDidAppearPartTwo;
+
+@end
